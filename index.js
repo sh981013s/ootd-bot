4988fef3cf548860905630b71189aa4c1877347b
sh981013s <sh981013s@gmail.com>
2022-11-29 01:27:55 +0900

    feat(index): !define(단어 도출) 명령어 로직 추가

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const fetch = (url) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(url));
+
+const PREFIX = "!";
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", (msg) => {
-  console.log(msg.content);
-  if (msg.content === "ping") {
-    msg.reply("pong");
+client.on("message", (message) => {
+  if (message.channel.type == "dm") return;
+  if (!message.content.startsWith(PREFIX)) return;
+
+  if (message.content.startsWith(PREFIX + "define")) {
+    const word = message.content.split(" ")[1];
+    getWordMeaning(word).then((data) => {
+      message.channel.send(data);
+    });
   }
 });
+
+const getWordMeaning = async (word) => {
+  const response = await fetch(
+    `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+  );
+  const data = await response.json();
+  return data[0].meanings[0].definitions[0].definition;
+};
 
 client.login(process.env.DISCORD_KEY);
